e7b32d61cfd5f8d843bd35102a71f2f81f2d4886
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2024-03-08 12:06:25 +0100

    user input for file path

--- git.py
+++ git.py
@@ -5,16 +5,19 @@ token = 'your_personal_token'
 
 repo_path = 'user/repo'
 
-file_path = 'file_path'
+file_path = input("Enter the filepath to your file that you want to upload:\n")
 
+# get file name from file_path
 file_name = file_path.split('/')[-1]
 
 g = Github(token)
 
 repo = g.get_repo(repo_path)
 
+# get current time for the tag in release
 release_tag = datetime.now().strftime("%H-%M-%S")
 
+# release name with date and time
 release_name = f'Release {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
 
 try:
@@ -22,9 +25,9 @@ try:
     
     asset = release.upload_asset(file_path, label=file_name)
     
+    # get ddl
     download_url = asset.browser_download_url
     
     print(f'Direct-Download-Link: {download_url}')
 except Exception as e:
     print(f"Error creating release: {e}")
-
